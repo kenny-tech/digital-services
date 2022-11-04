379036c4f0106faf23e5596157f74c47346820c6
kennytech <kennyemma2008@gmail.com>
2022-11-04 17:18:33 +0100

    Creating home page

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="row">
+            <div className="col-md-6" style={{ backgroundColor: '#ff0', height: '650px'}}>
+                <div>
+                    <h1>Buy Airtime</h1>
+                </div>
+            </div>
+            <div className="col-md-6" style={{ backgroundColor: '#B2BEB5', height: '650px'}}>
+                <div>
+                    <h1>Pay Bills</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
