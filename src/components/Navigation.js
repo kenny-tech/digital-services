53a16d0c5b9c247cd2dcfbc5d7893ab02a2a392d
kennytech <kennyemma2008@gmail.com>
2023-01-16 18:59:39 +0100

    Fixed navbar mobile view bug

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,15 +7,25 @@ const Navigation = () => {
             <Link className="navbar-brand" to="/">
                 Cakamba Digital Service
             </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse justify-content-lg-end" id="navbarText">
-                <Link to="/login">
-                    <span className="navbar-text">
-                        Login
-                    </span>
-                </Link>
+            <div className="collapse navbar-collapse justify-content-sm-end" id="navbarNavDropdown" >
+                <ul className="navbar-nav">
+                {/* <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li> */}
+                    <li className="nav-item">
+                        <Link to="/register">
+                            <a className="nav-link">Register</a>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/login">
+                            <a className="nav-link">Login</a>
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
